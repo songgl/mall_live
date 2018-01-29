87317a7b818b5164edace9d063a719800f4a3367
SGL <songgenling@SGLdeMacBook-Pro.local>
2018-01-29 18:00:39 +0800

    2018-1-29 修改api/order.js

--- src/api/order.js
+++ src/api/order.js
@@ -6,6 +6,9 @@ export default {
 		console.log(type)
 		return post('api/Order/queryOrderByState', data)
 	},
+	getTest () {
+		console.log(222)
+	},
 	// 获取订单详情
 	getOrderDetails (data) {
 		return post('api/Order/queryOrderView', data)

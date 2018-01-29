650614f6b206cfa02a8fd9c7be12ce776af917db
SGL <songgenling@SGLdeMacBook-Pro.local>
2018-01-29 16:30:28 +0800

    2018-1-29 修改api/order.js

--- src/api/order.js
+++ src/api/order.js
@@ -1,6 +1,10 @@
 import { post, get } from './config'
 
 export default {
+	// 获取订单列表
+	getOrderList (data) {
+		return post('api/Order/queryOrderByState', data)
+	},
 	// 获取订单详情
 	getOrderDetails (data) {
 		return post('api/Order/queryOrderView', data)

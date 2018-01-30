defedf10b01c38d98e0a2a9ec73a3091be2b5928
lx <lx@lxdeMacBook-Pro.local>
2018-01-30 10:21:08 +0800

    2018-1-30 order.js

--- src/api/order.js
+++ src/api/order.js
@@ -5,17 +5,6 @@ export default {
 	getOrderList (data) {
 		return post('api/Order/queryOrderByState', data)
 	},
-<<<<<<< HEAD
-	// test
-	getTest () {
-		console.log(222)
-	},
-    getTest222 () {
-		console.log(222)
-	},
-
-=======
->>>>>>> f85c18b04a0537b8e32e1bf98678e353b0d518d8
 	// 获取订单详情
 	getOrderDetails (data) {
 		return post('api/Order/queryOrderView', data)
